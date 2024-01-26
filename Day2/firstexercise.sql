dc741eaf1f3258ffe3a63d3c50a05960719d93f5
Nitha Rajkumar <Rajkumar_nithin@yahoo.com>
2024-01-26 15:36:09 +0530

    select and where statements

---
+++ Day2/firstexercise.sql
@@ -0,0 +1,16 @@
+use demo_database;
+CREATE TABLE employees1 (
+  ID int,
+  email VARCHAR(20),
+  fname VARCHAR(20),
+  department VARCHAR(20),
+  dob date,
+  salary float,
+  isactive bool
+);
+
+insert into employees1 values(1,'nit@gmail.com','nithin','HR','1995-01-10',30000,true);
+insert into employees1 values(2,'raju@gmail.com','raju','Testing','1992-01-10',40000,true);
+insert into employees1 values(3,'sam@gmail.com','sam','CS','1991-01-10',9900,true);
+select * from employees1;
+select ID,fname from employees1 where salary>10000;
